dd267785391a2f898eb0945611431cad98d94aef
Prathamesh Mali <prathamesh.mali@mosaicwellness.in>
2023-03-26 16:55:29 +0530

    feat: 🎸 add graphql queries for portfolio pages

---
+++ src/graphql/queries/index.ts
@@ -0,0 +1,15 @@
+import allCategories from "./allCategories.graphql";
+import allProjects from "./allProject.graphql";
+import homePage from "./homePage.graphql";
+import fetchProjectsByCategory from "./fetchProjectsByCategory.graphql";
+import aboutPage from "./aboutPage.graphql";
+import contactsPage from "./contactsPage.graphql";
+
+export {
+  allCategories,
+  allProjects,
+  homePage,
+  fetchProjectsByCategory,
+  aboutPage,
+  contactsPage,
+};
