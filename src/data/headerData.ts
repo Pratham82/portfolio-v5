44e5a3562618c034f9e58951b8d23cb312a962b8
Pratham82 <mali.prathamesh82@gmail.com>
2023-07-30 02:42:12 +0530

    feat: 🎸 added helpers

---
+++ src/data/headerData.ts
@@ -0,0 +1,22 @@
+export const headerLinks = [
+  { id: "a167c067-78e9-4e4f-9ff7-7255326c2a6c", link: "Home" },
+  { id: "b34f03b5-1f24-4b7e-9b9a-c43a830f8e35", link: "Projects" },
+  { id: "ffa62e68-d888-4df9-8bc2-9444d4eeb801", link: "Blogs" },
+  { id: "42d773e6-4999-4575-97f8-81c32be201c8", link: "Contact" },
+  { id: "56a0fc95-8999-4b54-8dd1-2c9bea902d4f", link: "About" },
+];
+
+export const socialLinks = [
+  {
+    id: "GITHUB",
+    link: "https://github.com/pratham82",
+  },
+  {
+    id: "LINKEDIN",
+    link: "https://www.linkedin.com/in/prathameshmali/",
+  },
+  {
+    id: "TWITTER",
+    link: "https://www.twitter.com/pratham_82 ",
+  },
+];
---
+++ src/helpers/index.ts
@@ -0,0 +1,19 @@
+const getCurrentHeadTitle = ({ pathname }: { pathname: string }) => {
+  const path = pathname.replace("/", "");
+  const pageTitle = path[0].toUpperCase() + path.slice(1) || "Pratham82";
+  return pageTitle;
+};
+
+const isCurrentPath = ({
+  link,
+  pathname,
+}: {
+  link: string;
+  pathname: string;
+}) => {
+  const currentLink = link.toLowerCase();
+  const [, cp] = pathname.split("/");
+  return currentLink === cp;
+};
+
+export { getCurrentHeadTitle, isCurrentPath };
