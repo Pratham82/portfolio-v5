2b89174ed273e0de9a9bc9097afc0b0456a30124
Pratham82 <mali.prathamesh82@gmail.com>
2023-08-19 12:37:59 +0530

    feat: 🎸 added animation in header tabs
    ✅ Closes: 000

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,58 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import classNames from "classnames";
 import Head from "next/head";
 
+import { motion } from "framer-motion";
+import { useState } from "react";
 import ThemeToggler from "./ThemeSwitcher";
-import { headerLinks } from "../src/data/headerData";
-import { isCurrentPath, getCurrentHeadTitle } from "../src/helpers";
+import { tabsData } from "../src/data/headerData";
+import { getCurrentHeadTitle } from "../src/helpers";
 
 const Header = () => {
-  const { pathname } = useRouter();
+  const { pathname, push } = useRouter();
+  const [activeTab, setActiveTab] = useState(tabsData[0].id);
 
   return (
     <>
       <Head>
         <title>{getCurrentHeadTitle({ pathname })}</title>
       </Head>
-      <div className="fixed top-0 z-10 w-full border-slate-400 py-4 shadow-sm shadow-gray-800 backdrop-blur-2xl">
+      <div className="z-11 fixed top-0 w-full border-slate-400 py-4 shadow-sm shadow-gray-800 backdrop-blur-2xl">
         <div className="mx-auto flex max-w-[280px] justify-between">
           <h1 className="text-center text-lg font-semibold">Pratham82</h1>
           <ThemeToggler />
         </div>
-        <div className="text mx-auto flex max-w-md list-none justify-center pt-4 text-sm dark:text-slate-400">
-          {headerLinks?.map(({ link, id }) => (
-            <div
-              key={id}
-              className={classNames("px-4 py-2", {
-                // "rounded-md bg-slate-800 font-medium text-white": isCurrentPath(
-                "rounded-md font-medium dark:text-slate-100": isCurrentPath({
-                  link,
-                  pathname,
-                }),
-              })}
+        <div className="space-x-1a flex justify-center pt-4">
+          {tabsData.map((tab) => (
+            <button
+              type="button"
+              key={tab.id}
+              onClick={() => {
+                setActiveTab(tab.id);
+                push(tab.id);
+              }}
+              className={`relative
+              px-3 py-1.5 text-sm font-medium transition focus-visible:outline-2 `}
+              style={{
+                WebkitTapHighlightColor: "transparent",
+              }}
             >
-              <Link href={`/${link.toLocaleLowerCase()}`}>{link}</Link>
-            </div>
+              {activeTab === tab.id && (
+                <motion.span
+                  layoutId="nav-pill"
+                  className="absolute inset-0 z-10 rounded-md bg-slate-900 dark:bg-slate-100"
+                  transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                />
+              )}
+              <span
+                className={classNames(
+                  "relative z-10 mix-blend-exclusion",
+                  activeTab === tab.id && "text-slate-50",
+                )}
+              >
+                {tab.label}
+              </span>
+            </button>
           ))}
         </div>
       </div>
--- src/data/headerData.ts
+++ src/data/headerData.ts
@@ -1,9 +1,24 @@
 export const headerLinks = [
-  { id: "a167c067-78e9-4e4f-9ff7-7255326c2a6c", link: "Home" },
-  { id: "b34f03b5-1f24-4b7e-9b9a-c43a830f8e35", link: "Projects" },
-  { id: "ffa62e68-d888-4df9-8bc2-9444d4eeb801", link: "Blogs" },
+  { id: "a167c067-78e9-4e4f-9ff7-7255326c2a6c", link: "Home", path: "home" },
+  {
+    id: "b34f03b5-1f24-4b7e-9b9a-c43a830f8e35",
+    link: "Projects",
+    path: "projects",
+  },
+  { id: "ffa62e68-d888-4df9-8bc2-9444d4eeb801", link: "Blogs", path: "blogs" },
   // { id: "42d773e6-4999-4575-97f8-81c32be201c8", link: "Contact" },
-  { id: "56a0fc95-8999-4b54-8dd1-2c9bea902d4f", link: "About" },
+  { id: "56a0fc95-8999-4b54-8dd1-2c9bea902d4f", link: "About", path: "about" },
+];
+
+export const tabsData = [
+  { label: "Home", id: "home" },
+  {
+    label: "Projects",
+    id: "projects",
+  },
+  { label: "Blogs", id: "blogs" },
+  // { id: "42d773e6-4999-4575-97f8-81c32be201c8", label: "Contact" },
+  { label: "About", id: "about" },
 ];
 
 export const socialLinks = [
